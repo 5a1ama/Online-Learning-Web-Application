d3158312e2390ce819efed39e8b2dc83b6228b3d
hazemheagzy1 <Hazem.Alhegazy@gmail.com>
2022-11-02 11:47:04 +0100

    hazem

--- frontend/src/components/courses/Courses.js
+++ frontend/src/components/courses/Courses.js
@@ -2,14 +2,35 @@ import React from 'react'
 import './Courses.css'
 export {default as Courses} from './Courses'
 function Courses() {
+  const Newcourse = (props) => (
+    <>
+     <div className="newCourse">
+      <h2>{props.course.title}</h2>
+      <h2 className='price'>{props.course.price}</h2>
+    </div>
+    </>
+  );
   return (
     <div name="courses">
+
       <h1 className="heading">My Courses</h1>
 
-    <div classname="newCourse">
+    <Newcourse course={{title:"CSEN103" , price:"100 $"}}/>
+
+    <div className="newCourse">
       <h2>CSEN 102</h2>
-      <h3>100$</h3>
+      <h2 className='price'>100$</h2>
     </div>
+
+     <div className="newCourse">
+      <h2>CSEN 102</h2>
+      <h2 className='price'>100$</h2>
+    </div> 
+    <div className="newCourse">
+      <h2>CSEN 102</h2>
+      <h2 className='price'>100$</h2>
+    </div>
+
     </div>
   )
 }
