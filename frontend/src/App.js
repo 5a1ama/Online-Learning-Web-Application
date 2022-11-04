1d4bf148dd6bae72c90bae5469bf7624f39744b2
hazemheagzy1 <Hazem.Alhegazy@gmail.com>
2022-11-04 17:00:38 +0100

    hazem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import {Routes, Route, useNavigate} from 'react-router-dom';
 import {Navbar} from './components/navbar/Navbar'
 import {Bgvid} from './components/bgvid/Bgvid'
 import {Courses} from './components/courses/Courses'
-import {AllCourses} from './components/courses/AllCourses'
+
 import { BiLogIn } from 'react-icons/bi';
 
 export default function App() {
