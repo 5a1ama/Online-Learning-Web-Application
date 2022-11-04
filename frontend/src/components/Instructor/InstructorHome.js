3fb5326c01d6d4d0e51e22164257dba69d3a1612
peterghatas <peter1ghatas@gmail.com>
2022-11-04 15:30:46 +0100

    peter

---
+++ frontend/src/components/Instructor/InstructorHome.js
@@ -0,0 +1,13 @@
+import Navbar from "../navbar/Navbar";
+import "./Instructor.css"
+
+export function InstructorHome(){
+    return(
+<div className = "divcenter">
+<div>
+
+        <Navbar items = {["HOME","My Courses"]} select = "HOME" />
+        </div>
+</div>
+    );
+}
