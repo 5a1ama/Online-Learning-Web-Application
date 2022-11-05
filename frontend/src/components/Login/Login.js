fb1361aa25e96fcd29817e4b17f6afb953708261
hazemheagzy1 <Hazem.Alhegazy@gmail.com>
2022-11-05 23:01:35 +0100

    hazem

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { BiNoEntry } from 'react-icons/bi'
-import { HiCubeTransparent } from 'react-icons/hi'
-import Navbar from '../navbar/Navbar'
+
 import './Login.css'
+import {Link} from 'react-scroll'
+
 export { default as Login } from './Login'
 
 function Login() {
@@ -13,16 +13,16 @@ div className = "login" >
              <h1 color = "white" > Learn. </h1> 
         </div>
          <div className = "LoginBox" >
-              <h2> Login Here </h2> 
+             <Link> <h2> Login Here </h2> </Link>
               <h3> Enter Your Email: </h3> 
-               <form className = "form" >
+               <form className = "Login-form" >
                <div>
                     <input type = "text" placeholder = "Ex: John@gmail.com" TextMode="Email" required="true"/ >
               </div>
               </form>
                     
                     <h3> Enter Your Password: </h3> 
-                    <form className = "form" >
+                    <form className = "Login-form" >
 
                     <input type = "text" placeholder = "**********" inputMode='password'  / >
                     </form>
