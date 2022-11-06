640dd035bf71b9d6f47e74020763b18da01746db
hazemheagzy1 <Hazem.Alhegazy@gmail.com>
2022-11-06 11:47:42 +0100

    hazem

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,40 +1,50 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import './Login.css'
 import {Link} from 'react-scroll'
 
+
 export { default as Login } from './Login'
 
 function Login() {
-    return ( <
-div className = "login" >
-
+    const [email,setEmail] = useState("");
+    const handleEmail = (event) => { setEmail(event.target.value)}
+    const [password,setPassword] = useState("");
+    const handlePassword = (event) => { setPassword(event.target.value)}
+    
+    return(<
+        div className = "login" >
         <div className = { 'logo' } >
             <a href="/">
             <h1 color = "white" > Learn. </h1> 
             </a>
         </div>
          <div className = "LoginBox" >
-             <Link> <h2> Login Here </h2> </Link>
-              <h3> Enter Your Email: </h3> 
-               <form className = "Login-form" >
-               <div>
-                    <input type = "text" placeholder = "Ex: John@gmail.com" TextMode="Email" required="true"/ >
-              </div>
+         <div className="LoginBox-content">
+         <Link> <h2> Login Here </h2> </Link>
+         <h3> Enter Your Email: </h3> 
+         <form className = "Login-form" >
+         <div>
+         <input type = "email" placeholder = "Ex: John@gmail.com" onChange={handleEmail} required={true}/ >
+         </div>
               </form>
                     
                     <h3> Enter Your Password: </h3> 
                     <form className = "Login-form" >
 
-                    <input type = "text" placeholder = "**********" inputMode='password'  / >
+                    <input type = "password" placeholder = "**********" inputMode='password' onChange={handlePassword} required={true}  / >
                     </form>
-
-         <div className = "SearchButtons" >
+                    
+                    <div className = "SearchButtons" >
             <button> Login</button>
          </div> 
-        
-
-        </div> 
+         <div className="Login-RegisterHere">
+         <h3 className="Login-NotReg">Not Registered yet?</h3>
+         <a href="/signUp"> Sign Up</a>
+         </div>
+         
+         </div> 
+         </div>
         </div>
     )
 }
