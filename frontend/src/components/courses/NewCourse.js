74842162af9030e75aab3fcefb0ec6268ccb4e8e
hazemheagzy1 <Hazem.Alhegazy@gmail.com>
2022-11-06 19:00:54 +0100

    hazem

--- frontend/src/components/courses/NewCourse.js
+++ frontend/src/components/courses/NewCourse.js
@@ -32,17 +32,30 @@ export function NewCourse(props) {
                     <br />
           <div>
               <h2 className='price'>{props.course.price}$</h2>
+              <h2 className='price2'>{props.course.discount}%</h2>
+
               {stars(props.course.rating).map((num)=> <img className="starImg" src={starImg} alt="."/>)}
           </div>
 
         <div className={courseDetails?"Large-NewData-NewCourse":"nonNewData-NewCourse"}>
+            <div style={{display:"flex" , flexDirection:"row"}}>
 
             <h3 style={{fontSize:"30px"}}>Course Content :</h3>
+            <h3 style={{fontSize:"30px", marginLeft:"8rem"}}>Excercises :</h3>
+
+            </div>
         <div className={courseDetails?"NewData-NewCourse":"nonNewData-NewCourse"}>
 
             <div className="Course-subTitles">
-                 {props.course.subtitles.map((sub,i)=>  <h4 >{i+1}  -  {sub}</h4>)}
+                 {props.course.subtitles.map((sub,i)=>  <h4 >{i+1}  -  {sub.name}</h4>)}
             </div>
+            <div className="Course-subTitles">
+                 {props.course.subtitles.map((sub,i)=>  <h4 > {sub.hours} Hours</h4>)}
+            </div>
+            <div className="Course-subTitles">
+                 {props.course.excercises.map((exe,i)=>  <h4 > {exe} </h4>)}
+            </div>
+            
         </div>
           </div>
         </div>
