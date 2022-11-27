0ecc120d5df6ea14e4fa237e36ce7c4784a20ad4
peterghatas <peter1ghatas@gmail.com>
2022-11-27 17:45:40 +0100

    pp

--- frontend/src/components/Trainee/TraineeHome.js
+++ frontend/src/components/Trainee/TraineeHome.js
@@ -3,13 +3,16 @@ import {useNavigate} from 'react-router-dom';
 import "./TraineeHome.css";
 
 
-export function TraineeHome(){
+export function TraineeHome (){
     const navigate = useNavigate(); 
     return(
         <div>
   
         <div>
-            <Navbar items={["Home","My Courses","All Courses"]} select="Home" nav={["/TraineeHome","/TraineeCourses","/TraineeAllCourses"]} scroll={["","",""]}  />
+             <Navbar items={["Home","My Courses","All Courses"]} select="Home" nav={["/TraineeHome","/TraineeCourses","/TraineeAllCourses"]} scroll={["","",""]}  />
+        </div>
+        <div className="profileDetails">
+        
         </div>
         </div>
     );
