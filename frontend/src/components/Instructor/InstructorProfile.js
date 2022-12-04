e638935036bf6f9a17597a9cf4bd1592ab94ef95
peterghatas <peter1ghatas@gmail.com>
2022-12-04 17:50:25 +0100

    peter

--- frontend/src/components/Instructor/InstructorProfile.js
+++ frontend/src/components/Instructor/InstructorProfile.js
@@ -79,58 +79,23 @@ export function InstructorProfile(){
     
     intial()
     return(
-        <div className='mainDiv'>
-            <Navbar items={["Home","My Courses","Caleneder"]} select="" nav={["/instructorHome","/InstructorCourses",""]} scroll={["","",""]}  />
-            <br></br>
-            <div className="profileData">
-            <Avatar  
+      <div>
+        <div>
+        <Navbar items={["Home","My Courses","Caleneder"]} select="Home" nav={["/instructorHome","/InstructorCourses",""]} scroll={["","",""]}  />
+        </div>
+        <div className="instructorDitails">
         
-        sx={{ backgroundColor: '#0277bd' ,width: 100, height: 100 ,fontSize:35}}
-        >
-            {instructor && ((instructor.Name.split(" ").lenght>=2) ?instructor.Name.substring(0,1)+instructor.Name.split(" ")[1].substring(0,1):instructor.Name.substring(0,1))}
-            
-        </Avatar>
-          <div className='details'>
-            <div className='editable'> <h5 className="instructorname">{instructor && instructor.Name}</h5> 
-            <EditIcon className="editIconClick" onClick={handleName} />
-            </div> 
-          
-           <div className='editable'><h5 className="instructorEmail">{instructor && instructor.Email}</h5>
-           <EditIcon className="editIconClick" onClick={handleEmail} />
-           </div>
-           <div className='editable'><h5 className="instructorEmail">{instructor && instructor.specialization}</h5>
-           <EditIcon className="editIconClick" onClick={handleSpecialization} />
-           </div>
-          {instructor && <Rating className="instructorRating"
-       name="half-rating-read" defaultValue={instructor && ((instructor.rating.value >Math.floor(instructor.rating.value) && instructor.rating.value<(Math.floor(instructor.rating.value)+0.5))? Math.floor(instructor.rating.value):Math.floor(instructor.rating.value)+0.5)} precision={0.5} readOnly />}
 
-          </div>
-          {specializationEdit && <div className='editSpecialization'><input onChange={handleNewSpec} className='editInput' placeholder='enter new Specialization'/>
-          <button style={{backgroundColor:"green"}} onClick={handleUpdate}>Confirm</button>
-          <button style={{backgroundColor:"red"}} onClick={()=>setSpecEdit(false)} >Cancel</button>
-          </div>}
-          {nameEdit && <div className='editSpecialization'><input onChange={handleNewName} className='editInput' placeholder='enter new name'/>
-          <button style={{backgroundColor:"green"}} onClick={handleUpdate}>Confirm</button>
-          <button style={{backgroundColor:"red"}} onClick={()=>setNameEdit(false)} >Cancel</button>
-          </div>}
-          {emailEdit && <div className='editSpecialization'><input onChange={handleNewEmail} className='editInput' placeholder='enter new Email'/>
-          <button style={{backgroundColor:"green"}} onClick={handleUpdate}>Confirm</button>
-          <button style={{backgroundColor:"red"}} onClick={()=>setEmailEdit(false)} >Cancel</button>
-          </div>}
-            </div>
-            <div>
-                <button onClick={handleChangePass}> Change Password</button>
-                <br></br>
-                <br></br>
-                {changePass && <div className='changePassDiv'>
-                    <input onChange={handleOldPass} type="password" placeholder="old password" />
-                    <input onChange={handleNewPass} type="password" placeholder="new password" />
-                    <input onChange={handleConfirmPass} type="password" placeholder="confirm new password" />
-                    <button style={{backgroundColor:"green"}} onClick={handleUpdatePass}>Confirm</button>
-                    <button style={{backgroundColor:"red"}} onClick={()=>setChangePass(false)} >Cancel</button>
-
-                    </div>}
-            </div>
+        <Avatar  
+       className="avatar"
+       sx={{ backgroundColor: '#0277bd' ,width: 100, height: 100 ,fontSize:55}}
+       >
+            {instructor && instructor.Name.substring(0,1)} 
+           
+       </Avatar>
+          <h5 className="instructorname">{instructor && instructor.Name}</h5>
+          <h5 className="instructorEmail">{instructor && instructor.Email}</h5>
+                 </div>
         </div>
     )
 }
