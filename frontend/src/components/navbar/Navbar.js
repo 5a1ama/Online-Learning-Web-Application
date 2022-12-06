8195be37165da01ca453faeb81c35522fa9d6ced
hazemheagzy1 <Hazem.Alhegazy@gmail.com>
2022-12-06 09:46:14 +0100

    Hazem

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -122,6 +122,7 @@ function Navbar(props) {
                 style={{borderRadius:'5px',marginRight:'0.5rem',cursor:'pointer'}} ></img>
                 <BiDownArrow className="icon" style={{marginRight: '1rem'}} onClick={()=>{handleAll();handleCountryBar()}}></BiDownArrow>
             </div>
+            
             <BiSearch className="icon" onClick={ ()=>{handleAll();handleSearchBar()}} style={{marginRight: '1rem'}}/>
             
             {localStorage.getItem("token")===null ? 
