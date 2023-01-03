cb54afb5aabfdc867a84933ee10dbea259a4ddc6
OmarAtef99 <73483190+OmarAtef99@users.noreply.github.com>
2023-01-03 11:13:13 +0100

    atef

--- frontend/src/components/Instructor/InstructorCreateExercise.js
+++ frontend/src/components/Instructor/InstructorCreateExercise.js
@@ -4,96 +4,43 @@ import { TextField } from '@mui/material';
 
 import "./InstructorCreateExercise.css"
 import { useState } from "react";
+import { QuestionsDiv } from "./QuestionsDiv";
 
 export function InstructorCreateExercise (){
     const [questionsArr,setQuestionsArr]=useState([""]);
     const [choicesArr,setChoicesArr]=useState([["","","",""]]);
-    const handleQuestion=(event)=>{
+    const [update,setUpdate]=useState("")
+    const handleQuestion=(index,value)=>{
+            
+            questionsArr[index]=value
+            setQuestionsArr(questionsArr);
+            setUpdate("")
+            
 
     }
-    const handleChoice=(event)=>{
-
+    const handleChoice=(index1,index2,value)=>{
+        choicesArr[index1][index2]=value
+        setChoicesArr(choicesArr)
     }
 
     const handleNewQuestion =()=>{
-        
-        var x=questionsArr;
-        x.push("");
-        setQuestionsArr(x);
-        var y=choicesArr
-        y.push(["","","",""]);
-        setChoicesArr(y);
+        setQuestionsArr(questionsArr.concat([""]));
+        setChoicesArr(choicesArr.concat([["","","",""]]));
         alert(questionsArr.length)
     }
 
-    const   QuestionsDiv =(props )=>{
-        return (
-            <div className="questionDiv">
-            <TextField
-                                type={"text"}
-                                className='atoofachoicescheckboxes'
-                                 id="outlined-basic" 
-                                 label="enter the question"
-                                 variant="outlined"
-                                  
-                                  size="small"
-                                  />
-            <div>
-            <TextField
-                                type={"text"}
-                                className='atoofachoicescheckboxes'
-                                 id="outlined-basic" 
-                                 label="choice 1"
-                                 variant="outlined"
-                                  
-                                  size="small"
-                                  />
-            <TextField
-                                type={"text"}
-                                className='atoofachoicescheckboxes'
-                                 id="outlined-basic" 
-                                 label="choice 2"
-                                 variant="outlined"
-                                  
-                                  size="small"
-                                  />
-            <TextField
-                                type={"text"}
-                                className='atoofachoicescheckboxes'
-                                 id="outlined-basic" 
-                                 label="choice 3"
-                                 variant="outlined"
-                                  
-                                  size="small"
-                                  />
-            <TextField
-                                type={"text"}
-                                className='atoofachoicescheckboxes'
-                                 id="outlined-basic" 
-                                 label="choice 4"
-                                 variant="outlined"
-                                  
-                                  size="small"
-                                  />
-            </div>
-            
-            
-            </div>
-        )
-    }
     return(
         <div>
             <Navbar items={["Home","My Courses","All Courses"]}     handleCountryNumber={()=>12}
             select="My Courses" nav={["/instructorHome","/InstructorCourses","/InstAllCourses"]} inst={true} scroll={["","",""]}  />
 <div className="QuestionsMainDiv">
+    <h1>Questions of The Excercise</h1>
     <button className="plusbuttn" onClick={handleNewQuestion}> 
         
         +
     </button>
    
-    {
-        questionsArr.map((question)=><QuestionsDiv/>)
-    }
+    <QuestionsDiv handleChoice2={handleChoice} handleQuestion2={handleQuestion} arr1={questionsArr} arr2={choicesArr}  />
 
 
 </div>
