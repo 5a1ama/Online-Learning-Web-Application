2323896019a0ff2df0868b88c4efc535bb910a79
hazemheagzy1 <Hazem.Alhegazy@gmail.com>
2023-01-17 23:27:32 +0100

    hazem

--- frontend/src/components/Trainee/TraineeHome.js
+++ frontend/src/components/Trainee/TraineeHome.js
@@ -5,10 +5,12 @@ import { NewCourse } from '../courses/NewCourse';
 import Avatar from '@mui/material/Avatar';
 import EditIcon from '@mui/icons-material/Edit';
 
-import {getTraineeCourses, getTraineeDetails} from '../../API/TraineeAPI';
+import {getTraineeCourses, getTraineeDetails, updateTraineeName} from '../../API/TraineeAPI';
 import "./TraineeHome.css";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { verify } from "../../API/LoginAPI";
+import { Divider, TextField } from "@mui/material";
+import Loading from "../loading/Loading";
 
 
 
@@ -59,7 +61,21 @@ export function TraineeHome (){
   const handleCountryNumber = (x) =>{
     setCountryNumber(x);
   }
+  const [TraineeName,setTraineeName] = useState("");
+    const handleTraineeName = (event)=>{
+      setTraineeName(event.target.value)
+    }
 
+    const handleSubmitTraineeData = async()=>{
+      const x = await updateTraineeName(TraineeName)
+      alert("Thank you, your account is now ready")
+    }
+    const handleLogOut = () => {
+      localStorage.clear();
+     //  Cookies.remove('Token');
+      navigate("/Login");
+      
+     }
  
     return(
         <div className = "TraineeHomeMain">
@@ -69,7 +85,35 @@ export function TraineeHome (){
                handleCountryNumber={handleCountryNumber}
                select="Home" nav={["/TraineeHome","/TraineeCourses","/TraineeAllCourses"]} trainee={true} scroll={["","",""]}  />
         
+                {details && !details.Name && <div className="InstHome_Overlay"></div>}
 
+        { details && !details.Name && 
+                <div className="reportInstructorDivShadowHome" >
+                      <div className="ShowPriceDiv" style={{transform:'translate(0rem,-60px)'}}>
+                            <h1 className="ShowPriceLabel" style={{fontSize:'15px',margin:'1rem'}}>please complete your data to initialize your profile</h1>
+                            <Divider className='' variant="middle"/>
+
+                            <TextField
+                            className="PriceTextField"
+                            sx={{width:'70%',margin:'1rem'}}
+                            id="outlined-multiline-flexible"
+                            label="Your name"
+                            // defaultValue={details[0]&&Math.floor(details[0].price*fares[chosenCountry])}
+                            multiline
+                            maxRows={7}
+                            onChange={handleTraineeName}
+                            />
+                             
+                            <div className='flexCol FromButtonsAddPrice'>
+                                    <button className="Inst_SetPrice" onClick={handleSubmitTraineeData} style={{margin:'0.5rem'}}> Submit</button>
+                                  
+                                    <button className="Inst_SetPrice" style={{backgroundColor:'#888',margin:'0.5rem'}} onClick={handleLogOut}> Logout</button>
+                                  
+                            </div>
+                        </div> 
+        </div>
+        }
+        
         <div className="mainDetailsTrainee">
 
           <div className="homeCoursesTrainee">
@@ -79,16 +123,20 @@ export function TraineeHome (){
             <div className="homeCoursesTrainee_Course">
           <div className="homeCoursesTrainee_Course_2">
 
-
+          
           {courses.map((course) => <NewCourse course={course} Trainee={details&&details.type}   country={countryNumber}/>)}
           </div>
             </div>
           </div>
 
 
+        
         <div className="traineeDitails">
-        <Avatar className="TraineeAvatar"
-       sx={{ backgroundColor: '#0277bd' ,width: 100, height: 100 ,fontSize:55}}
+        {
+          details?
+          <>
+          <Avatar className="TraineeAvatar"
+          sx={{ backgroundColor: '#0277bd' ,width: 100, height: 100 ,fontSize:55}}
         >
           { details.Name && details.Name.substring(0,1) }
             {/* {instructor && instructor.Name.substring(0,1)+instructor.Name.split(" ")[1].substring(0,1)} */}
@@ -99,7 +147,10 @@ export function TraineeHome (){
            <button className="traineeAccountCircleButton" onClick={() => navigate('/TraineeProfile')}>
        <AccountCircleIcon  color="primary" sx={{ fontSize: 35  }} className="AccountIconClick"/>
        </button>
-       
+          </>
+       :
+       <Loading></Loading>
+      }
         </div>
         </div>
       
